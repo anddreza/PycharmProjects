6b4538c5622b58376561cef80927bc3583488475
Andreza <dedezasilva@live.com>
2023-08-18 20:55:04 -0300

    feat: passagem de variaveis por parametro

--- flask_example_01/main.py
+++ flask_example_01/main.py
@@ -12,9 +12,18 @@ def index():
 def teste():
     return "<h1>Outra página de teste</h1>"
 
+# função hello
+@app.route("/hello")
+@app.route("/hello")
+@app.route("/hello/<nome>")
+def hello(nome):
+    return "<h2> Hello {}</h2>".format(nome)
+
 @app.route("/debug")
 def debug():
-    return "<h1>Apresentando debug</h1>"
+    return "<h1>Apresentando debug usando RULES</h1>"
+
+app.add_url_rule("/debug", "Debug", debug)
 
 if __name__ == '__main__':
     app.run(debug=True, port=3000)

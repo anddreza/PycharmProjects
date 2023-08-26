6b1a90093c7f4a0fbb8a204559c30253326d4683
Andreza <dedezasilva@live.com>
2023-08-25 21:06:53 -0300

    feat: alteração de informações referente a página criada

--- aulaDeMetodos/main.py
+++ aulaDeMetodos/main.py
@@ -6,8 +6,17 @@ app = Flask(__name__, static_folder='public')
 @app.route("/add", methods=["POST", "GET"])
 def add():
    if(request.method == "POST"):
+       # primeira forma
        usuario = request.form['nome']
-       senah = request.form['senha']
+       senha = request.form['senha']
+       print(usuario)
+       print(senha)
+
+       # segunda forma
+       data = request.form
+       u2 = data['nome']
+       s2 = data['senha']
+       return "ok - POST: %s"
    elif request.method == "GET":
        return "ok - GET"
 

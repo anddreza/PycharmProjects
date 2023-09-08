e7171c0c4707ae1871e58f600f359aae8450f994
Andreza <dedezasilva@live.com>
2023-09-08 19:36:20 -0300

    feat: criação do usuário

--- aulaClasses/main.py
+++ aulaClasses/main.py
@@ -1,5 +1,11 @@
 class Usuario:
-    def __init__(self, nome, idade):
-    
+    def __init__(self):
+        self.nome = ""
+        self.idade = 0
+
 if __name__ == '__main__':
-    print('PyCharm')
+    user = Usuario()
+    user.nome = "Andreza"
+    user.idade = 27
+    print("Nome" + user.nome + " , idade " + str(user.idade))
+    print(f"Nome: {user.nome}, idade: {user.idade}")

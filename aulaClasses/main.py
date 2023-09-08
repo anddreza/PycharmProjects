375ac03a44223aaeb8633c024851570e5eb9b654
Andreza <dedezasilva@live.com>
2023-09-08 19:54:38 -0300

    feat: alteração na ordem das informações especificando os param

--- aulaClasses/main.py
+++ aulaClasses/main.py
@@ -9,6 +9,8 @@ if __name__ == '__main__':
     #user.nome = "Andreza"
     #user.idade = 27
 
+    user_two = Usuario(idade=32, nome="Fernanda")
+
     print("Nome" + user.nome + " , idade " + str(user.idade))
     print(f"Nome: {user.nome}, idade: {user.idade}")
 

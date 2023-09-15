c91b5fc84c05e56df0bf3980a9564337e5f4457e
Andreza <dedezasilva@live.com>
2023-09-15 19:12:34 -0300

    feat: update dá aula de classes

--- aulaClasses/main.py
+++ aulaClasses/main.py
@@ -7,10 +7,14 @@ if __name__ == '__main__':
 
     user.nome = "Andreza"
     user.__update = 20
-    
+
+    delattr(user, 'nome')
+    setattr(user, 'nome', 'Thiago') #seta o atributo nome
+    setattr(user, 'nome_2', 'Joao')
+
     user_two = Usuario(idade=32, nome="Fernanda")
 
-    print("Nome" + user.nome + " , idade " + str(user.idade))
+    print("Nome" + user.nome + " , idade " + str(user.idade) + " update " + str(user.__update))
     print(f"Nome: {user.nome}, idade: {user.idade}")
 
     user.boas_vindas()

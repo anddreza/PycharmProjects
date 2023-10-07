6aaa87eea563f4d632fb23fecf00cce80127d353
Andreza <dedezasilva@live.com>
2023-10-06 21:43:59 -0300

    feat: inserção de novas funções

--- aprendendoCRUD/main.py
+++ aprendendoCRUD/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, template_folder='templates')
@@ -8,13 +8,13 @@ db = SQLAlchemy(app)
 class Estudantes(db.Model):
     id = db.Column('estudante_id', db.Integer, primary_key=True)
     nome = db.Column(db.String(100))
-    endereco = db.Column(db.String(100))
+    cidade = db.Column(db.String(100))
     email = db.Column(db.String(100))
     pin = db.Column(db.String(100))
 
-    def __init__(self, nome, endereco, email, pin):
+    def __init__(self, nome, cidade, email, pin):
         self.nome = nome
-        self.endereco = endereco
+        self.cidade = cidade
         self.email = email
         self.pin = pin
 
@@ -24,8 +24,17 @@ def show_all():
 
 @app.route('/new', methods=['GET', 'POST'])
 def new():
-    if request.
-
+    if request.method == 'POST':
+        if not request.form['nome'] or not request.form['cidade'] or not request.form['email'] or not request.form['pin']:
+            flash('Preencha todos os campos', 'Erro')
+        else:
+            estudante = Estudantes(request.form['nome'], request.form['cidade'], request.form['email'], request.form['pin'])
+            db.session.add(estudante)
+            db.session.commit()
+            flash('Estudante salvo', 'Sucesso')
+            from flask import redirect
+            return redirect(url_for('show_all'))
+        return render_template('new.html')
 if __name__ == 'main':
     app.app_context().push()
     db.create_all()

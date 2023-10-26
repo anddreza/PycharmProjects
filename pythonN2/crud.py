1591d0c96499095d4abbd73cbdf7f7ccf27cb616
Andreza <dedezasilva@live.com>
2023-10-26 14:44:19 -0300

    feat: informações de detalhes

--- pythonN2/crud.py
+++ pythonN2/crud.py
@@ -5,22 +5,24 @@ from flask_sqlalchemy import SQLAlchemy
 import time
 app = Flask(__name__, template_folder='public')
 ##URI para caminho até o banco de dados, se fosse um BD mais robusto ele teria uma string de conexão 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///storage.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///storage.db' #conexão com o banco de dados
 db = SQLAlchemy(app)
-
+# desde que você tem as classes relacioandas ao banco, o ORM faz o processo de criar, recuperar e converter
 class Pessoas(db.Model):
+    # definição dos tipos de dados do banco
     id = db.Column('id', db.Integer, primary_key=True, autoincrement=True)
     nome = db.Column(db.String(100))
     email = db.Column(db.String(100))
     telefone = db.Column(db.Integer)
     observacao = db.Column(db.String(100))
 
+    #objeto
     def __init__(self, nome, email, telefone, observacao):
         self.nome = nome
         self.email = email
         self.telefone = telefone
         self.observacao = observacao
-
+#serialização, se tiver problemas ao serializar em JSON os objetos, faça:
     def to_dict(self):
         return {
         'nome':self.nome,
@@ -33,10 +35,10 @@ class Pessoas(db.Model):
     @staticmethod
     def populate_db():
         lista_pessoas = []
-        # Aqui estou criando 100 usuários
+        # Aqui estou criando 100 usuários, mas a ideia era ser 10k
         for i in range(100):
             lista_pessoas.append(Pessoas(
-                nome='Usuário Teste',
+                nome='Usuario Teste',
                 email='teste@gmail.com',
                 telefone=i,
                 observacao='i'))
@@ -44,54 +46,26 @@ class Pessoas(db.Model):
         db.session.add_all(lista_pessoas)
         db.session.commit()
 
-@staticmethod
-def toJSON():
-        print(db.session.query(Pessoas).all())
-        return db.session.query(Pessoas).all()
-
-
+# verificação do tempo gasto para criar isso e alteração para json
 @app.route('/')
 def show_all():
+
+    inicio = time.time()
+    Pessoas.populate_db()
+
     pessoas = Pessoas.query.all()
     pessoas_to_dict = []
     for e in pessoas:
         pessoas_to_dict.append(e.to_dict())
 
     aux = json.dumps(pessoas_to_dict)
-    return render_template('show_all.html', pessoas=aux)
-@app.route('/success/<tempo>')
-def success(tempo):
-    print(jsonify(Pessoas.toJSON()))
-    return 'Tempo: %s' %tempo
 
-@app.route('/adicionaPessoas')
-def tempo():
-    inicio = time.time()
-    Pessoas.populate_db()
     fim = time.time()
     tempo = (fim - inicio) * 1000
     print(fim - inicio)
-    return redirect(url_for('success', tempo=(fim - inicio) * 1000))
-
+    return render_template('show_all.html', pessoas=aux, x=tempo)
 
 if __name__ == '__main__':
     app.app_context().push() # recupera o contexto da aplicação que está usando o banco
     db.create_all() # cria todas as instâncias da database se não existirem
     app.run(host="0.0.0.0", port="5000", debug=True)
-    #Pessoas.populate_db()
-
-    # @app.route('/new', methods=['GET', 'POST'])
-    # def new():
-    #     if request.method == 'POST':
-    #         if not request.form['nome'] or not request.form['email'] or not request.form['telefone'] or not request.form['observacao']:
-    #             flash('Preencha todos os campos', 'Erro')
-    #         else:
-    #             estudante = Pessoas(request.form['nome'], request.form['email'], request.form['telefone'], request.form['observacao'])
-    #             db.session.add(pessoas)
-    #             db.session.commit()
-    #             flash('Usuário salvo!', 'Sucesso')
-    #             from flask import redirect
-    #             return redirect(url_for('show_all'))
-    #         return render_template('new.html')
-    #
-    # app.add_url_rule("/debug", "Debug", debug)
